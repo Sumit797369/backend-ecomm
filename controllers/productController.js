2bb1dec579bf3b4a55599d54871051190894115f
Sumit Kumar <tbeast935@gmail.com>
2026-04-05 00:09:00 +0530

    contoller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,36 @@
+const addProduct = (req, res) => {
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller,
+    } = req.body;
 
+    const image1 = req.files.image1[0]
+    const image2 = req.files.image2[0]
+    const image3 = req.files.image3[0]
+    const image4 = req.files.image4[0]
 
-const addProduct =(req,res)=>{
+    console.log( name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestseller);
     
+console.log(image1,image2,image3,image4);
 
-}
+  } catch (error) {}
+};
 
-const listProduct =(req,res)=>{
+const listProduct = (req, res) => {};
 
+const removeProduct = (req, res) => {};
+const singleProduct = (req, res) => {};
 
-}
-
-const removeProduct =(req,res)=>{
-
-
-}
-const singleProduct =(req,res)=>{
-
-
-}
-
-export{addProduct,listProduct,removeProduct,singleProduct}
+export { addProduct, listProduct, removeProduct, singleProduct };
