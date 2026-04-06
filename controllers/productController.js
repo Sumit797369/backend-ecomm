9fb2537bba009dbe91cd0654152f8c84b003c067
Sumit Kumar <tbeast935@gmail.com>
2026-04-07 00:21:35 +0530

    pc

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,6 @@
-const addProduct = (req, res) => {
+import {v2 as cloudinary} from "cloudinary";
+
+const addProduct = async (req, res) => {
   try {
     const {
       name,
@@ -10,10 +12,20 @@ const addProduct = (req, res) => {
       bestseller,
     } = req.body;
 
-    const image1 = req.files.image1 && req.files.image1[0]
-    const image2 = req.files.image2 && req.files.image2[0]
-    const image3 = req.files.image3 && req.files.image3[0]
-    const image4 = req.files.image4 && req.files.image4[0]
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image4 && req.files.image4[0];
+
+    const images= [image1,image2,image3,image4].filter((item)=> item !== undefined);
+    let imageUrl = await Promise.all(
+      images.map(async (item)=>{
+        let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+        return result.secure_url
+
+      })
+    )
+
 
     console.log( name,
       description,
