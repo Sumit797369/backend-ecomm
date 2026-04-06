a5224e762b2868caa9466c62cef60176803fdb73
Sumit Kumar <tbeast935@gmail.com>
2026-04-07 00:06:25 +0530

    cloudinary

--- server.js
+++ server.js
@@ -2,16 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import 'dotenv/config'
 import connectDB from './config/mongodb.js';
-import connectUploadCare from './config/uploadCare.js';
+import connectUploadCare from './config/cloudinary.js';
 import userRouter from './routes/userRoute.js';
 import productRouter from './routes/productRoute.js';
+import connectCloudinary from './config/cloudinary.js';
 
 //App config
 const app = express()
 const port = process.env.PORT || 4000
 connectDB();
-connectUploadCare(); 
-// console.log(process.env.MONGODB_URI);
+connectCloudinary()
+
 
 
 //middlewares
